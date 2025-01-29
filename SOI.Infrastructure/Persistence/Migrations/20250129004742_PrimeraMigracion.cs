a2aa3e4d92e4c773bd79085738e0e47604273cfc
FranciscoWatson <francisco@watson.com.ar>
2025-01-28 21:51:11 -0300

    Agrego Primera Migracion

---
+++ SOI.Infrastructure/Persistence/Migrations/20250129004742_PrimeraMigracion.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SOI.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class PrimeraMigracion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cuentas",
+                columns: table => new
+                {
+                    CuentaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cuentas", x => x.CuentaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Estados",
+                columns: table => new
+                {
+                    EstadoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estados", x => x.EstadoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TiposActivos",
+                columns: table => new
+                {
+                    TipoActivoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TiposActivos", x => x.TipoActivoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Activos",
+                columns: table => new
+                {
+                    ActivoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Ticker = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: false),
+                    TipoActivoId = table.Column<int>(type: "int", nullable: false),
+                    PrecioUnitario = table.Column<decimal>(type: "decimal(18,4)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Activos", x => x.ActivoId);
+                    table.ForeignKey(
+                        name: "FK_Activos_TiposActivos_TipoActivoId",
+                        column: x => x.TipoActivoId,
+                        principalTable: "TiposActivos",
+                        principalColumn: "TipoActivoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ordenes",
+                columns: table => new
+                {
+                    OrdenId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CuentaId = table.Column<int>(type: "int", nullable: false),
+                    ActivoId = table.Column<int>(type: "int", nullable: false),
+                    EstadoId = table.Column<int>(type: "int", nullable: false),
+                    Operacion = table.Column<string>(type: "nvarchar(1)", nullable: false),
+                    Cantidad = table.Column<int>(type: "int", nullable: false),
+                    Precio = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    MontoTotal = table.Column<decimal>(type: "decimal(18,4)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ordenes", x => x.OrdenId);
+                    table.ForeignKey(
+                        name: "FK_Ordenes_Activos_ActivoId",
+                        column: x => x.ActivoId,
+                        principalTable: "Activos",
+                        principalColumn: "ActivoId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Ordenes_Cuentas_CuentaId",
+                        column: x => x.CuentaId,
+                        principalTable: "Cuentas",
+                        principalColumn: "CuentaId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Ordenes_Estados_EstadoId",
+                        column: x => x.EstadoId,
+                        principalTable: "Estados",
+                        principalColumn: "EstadoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Activos_TipoActivoId",
+                table: "Activos",
+                column: "TipoActivoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ordenes_ActivoId",
+                table: "Ordenes",
+                column: "ActivoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ordenes_CuentaId",
+                table: "Ordenes",
+                column: "CuentaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ordenes_EstadoId",
+                table: "Ordenes",
+                column: "EstadoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ordenes");
+
+            migrationBuilder.DropTable(
+                name: "Activos");
+
+            migrationBuilder.DropTable(
+                name: "Cuentas");
+
+            migrationBuilder.DropTable(
+                name: "Estados");
+
+            migrationBuilder.DropTable(
+                name: "TiposActivos");
+        }
+    }
+}

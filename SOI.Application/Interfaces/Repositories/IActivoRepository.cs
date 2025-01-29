1b517aaab15b600bc478425e65ee419c31dd72d3
FranciscoWatson <francisco@watson.com.ar>
2025-01-29 18:13:38 -0300

    Agrego ActivoRepository y IActivoRepository

---
+++ SOI.Application/Interfaces/Repositories/IActivoRepository.cs
@@ -0,0 +1,12 @@
+﻿using SOI.Domain.Entities;
+
+namespace SOI.Application.Interfaces.Repositories;
+
+public interface IActivoRepository
+{
+    public Task<List<Activo>> GetAllAsync();
+    public Task<Activo?> GetByIdAsync(int id);
+    public Task<Activo> CreateAsync(Activo activo);
+    public Task UpdateAsync(Activo activo);
+    public Task DeleteAsync(int id);
+}
---
+++ SOI.Infrastructure/Persistence/Repositories/ActivoRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SOI.Application.Interfaces.Repositories;
+using SOI.Domain.Entities;
+
+namespace SOI.Infrastructure.Persistence.Repositories;
+
+public class ActivoRepository : IActivoRepository
+{
+    private readonly SoiDbContext _context;
+    
+    public ActivoRepository(SoiDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<List<Activo>> GetAllAsync()
+    {
+        return await _context.Activos.ToListAsync();
+    }
+
+    public async Task<Activo?> GetByIdAsync(int id)
+    {
+        return await _context.Activos.FindAsync(id);
+    }
+
+    public async Task<Activo> CreateAsync(Activo activo)
+    {
+        await _context.Activos.AddAsync(activo);
+        await _context.SaveChangesAsync();
+        return activo;
+    }
+
+    public async Task UpdateAsync(Activo activo)
+    {
+        _context.Activos.Update(activo);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var activo = await _context.Activos.FindAsync(id);
+        _context.Activos.Remove(activo);
+        await _context.SaveChangesAsync();
+    }
+}

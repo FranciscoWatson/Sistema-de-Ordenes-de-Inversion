3ff4114c291ef04a7652d584fac76f96ca3fe2e0
FranciscoWatson <francisco@watson.com.ar>
2025-01-28 21:42:41 -0300

    Remuevo una configuracion innecesaria

--- SOI.Infrastructure/Persistence/Configurations/ActivoConfiguration.cs
+++ SOI.Infrastructure/Persistence/Configurations/ActivoConfiguration.cs
@@ -8,7 +8,6 @@ public class ActivoConfiguration : IEntityTypeConfiguration<Activo>
 {
     public void Configure(EntityTypeBuilder<Activo> builder)
     {
-        builder.HasKey(a => a.ActivoId);
         builder.Property(a => a.Nombre).HasMaxLength(100);
         builder.Property(a => a.Ticker).HasMaxLength(32);
         builder.Property(a => a.PrecioUnitario).HasColumnType("decimal(18,4)");

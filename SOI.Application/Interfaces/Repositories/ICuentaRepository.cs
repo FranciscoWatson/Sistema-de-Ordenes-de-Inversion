d696855175a8bdb9704ad714e8b98766e17c0eeb
FranciscoWatson <francisco@watson.com.ar>
2025-01-29 20:06:33 -0300

    Agrego CuentaRepository

---
+++ SOI.Application/Interfaces/Repositories/ICuentaRepository.cs
@@ -0,0 +1,12 @@
+﻿using SOI.Domain.Entities;
+
+namespace SOI.Application.Interfaces.Repositories;
+
+public interface ICuentaRepository
+{
+    public Task<List<Cuenta>> GetAllAsync();
+    public Task<Cuenta?> GetByIdAsync(int id);
+    public Task<Cuenta> CreateAsync(Cuenta cuenta);
+    public Task UpdateAsync(Cuenta cuenta);
+    public Task DeleteAsync(int cuenta);
+}
---
+++ SOI.Infrastructure/Persistence/Repositories/CuentaRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SOI.Application.Interfaces.Repositories;
+using SOI.Domain.Entities;
+
+namespace SOI.Infrastructure.Persistence.Repositories;
+
+public class CuentaRepository : ICuentaRepository
+{
+    private readonly SoiDbContext _context;
+    
+    public async Task<List<Cuenta>> GetAllAsync()
+    {
+        return await _context.Cuentas.ToListAsync();
+    }
+
+    public async Task<Cuenta?> GetByIdAsync(int id)
+    {
+        return await _context.Cuentas.FindAsync(id);
+    }
+
+    public async Task<Cuenta> CreateAsync(Cuenta cuenta)
+    {
+        await _context.Cuentas.AddAsync(cuenta);
+        await _context.SaveChangesAsync();
+        return cuenta;
+    }
+
+    public async Task UpdateAsync(Cuenta cuenta)
+    {
+        _context.Cuentas.Update(cuenta);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var cuenta = await _context.Cuentas.FindAsync(id);
+        _context.Cuentas.Remove(cuenta);
+        await _context.SaveChangesAsync();
+    }
+}

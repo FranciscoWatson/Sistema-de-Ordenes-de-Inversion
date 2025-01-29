89ce8eb53bf5a76b8984c536db482c0f8a8389c8
FranciscoWatson <francisco@watson.com.ar>
2025-01-29 19:38:04 -0300

    Agrego CrearOrdenDto

---
+++ SOI.Application/DTOs/CrearOrdenDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SOI.Application.DTOs;
+
+public class CrearOrdenDto
+{
+    public int CuentaId { get; set; }
+    public int ActivoId { get; set; }
+    public int Cantidad { get; set; }
+    public decimal? Precio { get; set; }  
+    public char Operacion { get; set; }
+}

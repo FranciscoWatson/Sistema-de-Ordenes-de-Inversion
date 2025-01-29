3f578448dca8527ddc866b06f9ee4b8152c24c8d
FranciscoWatson <francisco@watson.com.ar>
2025-01-28 22:07:48 -0300

    Agrego IOrdenRepository

---
+++ SOI.Application/Interfaces/Repositories/IOrdenRepository.cs
@@ -0,0 +1,11 @@
+﻿using SOI.Domain.Entities;
+namespace SOI.Application.Interfaces.Repositories;
+
+public interface IOrdenRepository
+{
+    public Task<List<Orden>> GetAllAsync();
+    public Task<Orden> GetByIdAsync(int id);
+    public Task<Orden> CreateAsync(Orden orden);
+    public Task UpdateAsync(Orden orden);
+    public Task DeleteAsync(Orden orden);
+}

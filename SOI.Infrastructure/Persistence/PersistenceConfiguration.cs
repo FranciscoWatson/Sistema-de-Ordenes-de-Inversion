2950eea69b2ae33d9b0e97f543ac3ca6c8471094
FranciscoWatson <francisco@watson.com.ar>
2025-01-28 21:11:46 -0300

    Agrego DbContext y Persistence configuration

---
+++ SOI.Infrastructure/Persistence/PersistenceConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+
+namespace SOI.Infrastructure.Persistence;
+
+public static class PersistenceConfiguration
+{
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<SoiDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("SqlServerConnection"));
+        });
+
+        return services;
+    }
+}
---
+++ SOI.Infrastructure/Persistence/SoiDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SOI.Domain.Entities;
+
+namespace SOI.Infrastructure.Persistence;
+
+public class SoiDbContext : DbContext
+{
+    public SoiDbContext(DbContextOptions<SoiDbContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<Activo> Activos { get; set; }
+    public DbSet<Cuenta> Cuentas { get; set; }
+    public DbSet<Estado> Estados { get; set; }
+    public DbSet<Orden> Ordenes { get; set; }
+    public DbSet<TipoActivo> TiposActivos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(SoiDbContext).Assembly);
+    }
+}

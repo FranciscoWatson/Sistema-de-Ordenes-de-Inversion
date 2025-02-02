5d3fc65f6b1aa06fd80255b3937686690fd8542e
FranciscoWatson <francisco@watson.com.ar>
2025-02-02 14:08:52 -0300

    Agrego Feature: Validaciones para Actualizar Orden

---
+++ SOI.Application/Validations/ActualizarOrdenValidator.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using SOI.Application.Commands;
+
+namespace SOI.Application.Validations;
+
+public class ActualizarOrdenValidator : AbstractValidator<ActualizarOrdenCommand>
+{
+    public ActualizarOrdenValidator()
+    {
+        RuleFor(x => x.OrdenId)
+            .GreaterThan(0)
+            .WithMessage("El OrdenId debe ser mayor a 0.");
+
+        RuleFor(x => x.CuentaId)
+            .GreaterThan(0)
+            .WithMessage("La CuentaId debe ser mayor a 0.");
+
+        RuleFor(x => x.ActivoId)
+            .GreaterThan(0)
+            .WithMessage("El ActivoId debe ser mayor a 0.");
+
+        RuleFor(x => x.Cantidad)
+            .GreaterThan(0)
+            .WithMessage("La cantidad debe ser mayor a 0.");
+
+        RuleFor(x => x.Precio)
+            .GreaterThan(0)
+            .When(x => x.Precio.HasValue)
+            .WithMessage("El precio debe ser mayor a 0.");
+        
+        RuleFor(x => x.Operacion)
+            .NotEmpty()
+            .WithMessage("La operación es obligatoria.")
+            .Must(op => op == 'V' || op == 'C')
+            .WithMessage("La operación debe ser 'V' (Venta) o 'C' (Compra).");
+    }
+}
